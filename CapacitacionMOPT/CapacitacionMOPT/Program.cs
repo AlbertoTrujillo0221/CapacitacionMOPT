28b4f7cefc6191a9d17348ff287ca10a3f7178e2
Gabriela <gabriela.torres@mopt.go.cr>
2026-07-23 11:28:16 -0600

    Primer commit GTF

--- CapacitacionMOPT/CapacitacionMOPT/Program.cs
+++ CapacitacionMOPT/CapacitacionMOPT/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("GTF, Uno!");
+Console.WriteLine("GTF, Dos!");
+Console.WriteLine("GTF, Tres!");
+Console.WriteLine("GTF, Cuatro!");
+Console.WriteLine("GTF, Cinco!");
