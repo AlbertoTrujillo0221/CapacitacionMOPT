15a898513cbc35943d33b5cc85823a5cbcb3b880
Gustavo <gustavo.avendano@mopt.go.cr>
2026-07-23 11:34:03 -0600

    Primer commit de Gustavo.

--- CapacitacionMOPT/CapacitacionMOPT/Program.cs
+++ CapacitacionMOPT/CapacitacionMOPT/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Gustavo - Pagina 1");
+Console.WriteLine("Hello, Gustavo - Pagina 2");
+Console.WriteLine("Hello, Gustavo - Pagina 3");
+Console.WriteLine("Hello, Gustavo - Pagina 4");
