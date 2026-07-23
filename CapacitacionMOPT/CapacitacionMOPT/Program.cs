22e0033690bcfa38ca1e3b19bb270a3d8b612c4d
jay.brenes <jay.brenes@mopt.go.cr>
2026-07-23 11:43:07 -0600

    Segundo commit Jay

--- CapacitacionMOPT/CapacitacionMOPT/Program.cs
+++ CapacitacionMOPT/CapacitacionMOPT/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("1");
+Console.WriteLine("1");
+Console.WriteLine("1!");
+Console.WriteLine("1");
+Console.WriteLine("1");
