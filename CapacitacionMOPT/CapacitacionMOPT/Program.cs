6dbe0a20b36c7913ae12166245e9d75b6c95fd63
Fabian.varela <fabian.varela@mopt.go.cr>
2026-07-23 11:46:09 -0600

    primer comitt de fabian

--- CapacitacionMOPT/CapacitacionMOPT/Program.cs
+++ CapacitacionMOPT/CapacitacionMOPT/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Fabian!");
+Console.WriteLine("MOPT, Fabian!");
+Console.WriteLine("Prueba Fabian!");
+Console.WriteLine("Fabian, Fabian!");
+Console.WriteLine("Hola, Fabian!");
