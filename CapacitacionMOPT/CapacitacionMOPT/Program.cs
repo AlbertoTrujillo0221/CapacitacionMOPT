07970f988eb0baf131c8f137b657562061946e4b
Marisel <marisel.castro@mopt.go.cr>
2026-07-23 11:34:38 -0600

    Primer comit de Marisel

--- CapacitacionMOPT/CapacitacionMOPT/Program.cs
+++ CapacitacionMOPT/CapacitacionMOPT/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Marisel Castro");
+Console.WriteLine("Les saluda Marisel");
+Console.WriteLine("Marisel MOPT");
+Console.WriteLine("Prueba Marisel");
